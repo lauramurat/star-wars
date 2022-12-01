044e6e1718771339d8d06364c97c18273d5ffe54
Asus <laura.murat@narxoz.kz>
2022-12-01 17:48:49 +0600

    First Commit

---
+++ src/components/sw-character-layout/index.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+
+import { DefaultContext } from "../../Contexts";
+
+export const CharacterLayout = ({ item, children }) => {
+  const { handleChooseFan } = useContext(DefaultContext);
+  return (
+    <div key={item.name} className="item-list-layout">
+      <div>{children}</div>
+      <div className="item-image-layout">
+        <button
+          className="favorite-button"
+          onClick={() => handleChooseFan(item)}
+        >
+          I love your: !!
+        </button>
+        <img className="item-image" src={item.url} alt={item.name} />
+      </div>
+    </div>
+  );
+};
